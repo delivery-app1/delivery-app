788778a558f9c058d1bae75f29034d23aeaf5e11
baraahalomari <balomari995@gmail.com>
2021-07-14 19:04:42 +0300

    deletHandler

--- src/Components/header/index.js
+++ src/Components/header/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Navbar,Nav,Container} from 'react-bootstrap/'
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>ASAC Help Desk</li>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/admin">admin</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+   
+    <Navbar bg="dark" variant="dark">
+   
+    <Navbar.Brand href="/">Caffee House</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/admin">Orders</Nav.Link>
+    </Nav>
+
+  </Navbar>
+
+
   );
 }
 
--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './home.css';
-import {Form,Button} from 'react-bootstrap/';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import io from 'socket.io-client';
 const SERVER_URL = process.env.SERVER_URL || 'localhost:5000/';
@@ -10,21 +9,40 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      studentName: '',
+      customerName: '',
+      coffee: []
     };
   }
   componentDidMount() {
-    const studentName = prompt("WHAT's your name?");
-    this.setState({ studentName });
+    const customerName = prompt("WHAT's your name?");
+    this.setState({ customerName });
     this.props.socket.on('connect', () => {
       this.props.socket.on('claimed', function (payload) {
-        alert(`${payload.name} claimed your ticket`);
+        alert(`${payload.name} claimed your ordar`);
       });
     });
   }
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
+
+  handleChecked = (e) => {
+    if(e.target.checked){
+      this.setState({ 
+        coffee: [...this.state.coffee, e.target.value]
+       });
+    } else if(!e.target.checked && this.state.coffee.includes(e.target.value)){
+      this.setState({
+        coffee: this.state.coffee.filter(element=>{
+          return element !== e.target.value;
+        })
+      })
+    }
+  };
+
+
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -33,115 +51,117 @@ class Home extends React.Component {
       created_at: Date.now(),
     };
     console.log('hello', payload);
-    // once the user submit the form we need to emit a ticket so all TAs can see that ticket
-
-    // 1
+   
     this.props.socket.emit('createTicket', payload);
   };
   render() {
     return (
-        <>
-      <main className="container">
-         <section className="form-card">
-     
-           <form id="order-form" onSubmit={this.handleSubmit}>
-           <label >Notes</label>
-             <input
-               className="notes"
-               type="text"
-               name="notes"
-               placeholder="write your notes here..."
-               required
-               onChange={this.handleChange}
-             />
-            <br></br>
-             <label  >Coffee Menu</label>
-             <select name="coffee" id="coffee" onChange={this.handleChange}>
-               <option name="coffee" value="Americano">Americano</option>
-               <option name="coffee" value="Hot Coffee">Hot Coffee</option>
-               <option name="coffee" value="Ice Coffee">Ice Coffee</option>
-               <option name="coffee" value="Espresso">Espresso</option>
-               <option name="coffee" value="Mocha">Mocha</option>
-               <option name="coffee" value="Latte">Latte</option>
-             </select>
-             <label className="size">Drink size:</label>
-             <select name="size" id="size" onChange={this.handleChange}>
-               <option name="size" value="S">Small</option>
-               <option name="size" value="M">Medium</option>
-               <option name="size" value="L">Large</option>
-             </select>
-             <label className="branch">
-               <input
-                 type="radio"
-                 name="branch"
-                 value="Amman-Rainbow.St"
-                 required
-                 onChange={this.handleChange}
-               />
-               Amman-Rainbow.St
-             </label>
-             <label className="branch">
-               <input
-                 type="radio"
-                 name="branch"
-                 value="Amman Maka-Mall"
-                 onChange={this.handleChange}
-               />
-               Amman Mecca-Mall
-             </label>
-             <label className="branch">
-               <input
-                 type="radio"
-                 name="branch"
-                 value="Irbid City-Center"
-                 onChange={this.handleChange}
-               />
-               Irbid City-Center
-             </label>
-             <button className="order">submit</button>
-           </form> 
-             </section>
-             </main>
-    {/*  <Form onSubmit={this.handleSubmit}>
-      <Form.Group className="mb-3" >
-          <Form.Label>Notes</Form.Label>
-          <Form.Control className="notes" type="text" placeholder="write your notes here..." onChange={this.handleChange} />
-        </Form.Group>
-         <Form.Select name="coffee" id="coffee" onChange={this.handleChange}>
-          <option>Open this select menu</option>
-          <option name="coffee" value="Americano">Americano</option>
-          <option name="coffee" value="Hot Coffee">Hot Coffee</option>
-          <option name="coffee" value="Ice Coffee">Ice Coffee</option>
-          <option name="coffee" value="Espresso">Espresso</option>
-          <option name="coffee" value="Mocha">Mocha</option>
-          <option name="coffee" value="Latte">Latte</option>
-        </Form.Select>
+      <>
+        <main className="container">
+          <section className="form-card">
 
-        <Form.Select name="size"  onChange={this.handleChange}>
-        <option name="size" value="S">Smail</option>
-        <option name="size" value="M">Medium</option>
-               <option name="size" value="L">Large</option>
-               </Form.Select>
-               
-               <Form.Group className="coffee" controlId="formBasicPassword">
-               <Form.Label>Coffee Menu</Form.Label>
-          <Form.Control type="radio"
-          name="branch"
-                 value="Amman-Rainbow.St"
-                 required
-                 onChange={this.handleChange} />
-                 <Form.Control type="radio"
-                 type="radio"
-                 name="branch"
-                 value="Amman Maka-Mall"
-                 onChange={this.handleChange}/>
-                 </Form.Group>
-                
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-    </Form>*/}
-                </>
+            <form id="order-form" onSubmit={this.handleSubmit}>
+              <p>Coffee Mnue</p>
+              <br></br>
+              <label >Notes</label>
+              <input
+                className="notes"
+                type="text"
+                name="notes"
+                placeholder="write your notes here..."
+                required
+                onChange={this.handleChange}
+              />
+              <br></br>
+              <label  >Coffee Menu</label>
+
+              <br></br>
+              <label className="size">Americano</label>
+              <input
+                type="checkbox"
+                name="coffee"
+              
+                value="Amiricano"
+                onChange={this.handleChecked}
+              />
+
+              <label className="size">Hot Coffee</label>
+              <input
+                type="checkbox"
+                name="coffee"
+                value="Hot Coffee"
+                onChange={this.handleChecked}
+              />
+              <label className="size">Ice Coffee</label>
+              <input
+                type="checkbox"
+                name="Ice Coffee"
+                value="Ice Coffee"
+                onChange={this.handleChecked}
+              />
+              <label className="size">Espresso</label>
+              <input
+                type="checkbox"
+                name="Espresso"
+                value="Espresso"
+                onChange={this.handleChecked}
+              />
+              <label className="size">Mocha</label>
+              <input
+                type="checkbox"
+                name="Mocha"
+                value="Mocha"
+                onChange={this.handleChecked}
+              />
+              <label className="size">Latte</label>
+              <input
+                type="checkbox"
+                name="Latte"
+                value="Latte"
+                onChange={this.handleChecked}
+              />
+              <div className="size">
+                <label className="size">Drink size:</label>
+                <select name="size" id="size" onChange={this.handleChange}>
+                  <option name="size" value="S">Small</option>
+                  <option name="size" value="M">Medium</option>
+                  <option name="size" value="L">Large</option>
+                </select>
+              </div>
+              <label className="branch">
+                <input
+                  type="radio"
+                  name="branch"
+                  value="Amman-Rainbow.St"
+                  required
+                  onChange={this.handleChange}
+                />
+                Amman-Rainbow.St
+              </label>
+              <label className="branch">
+                <input
+                  type="radio"
+                  name="branch"
+                  value="Amman Maka-Mall"
+                  onChange={this.handleChange}
+                />
+                Amman Mecca-Mall
+              </label>
+              <label className="branch">
+                <input
+                  type="radio"
+                  name="branch"
+                  value="Irbid City-Center"
+                  onChange={this.handleChange}
+                />
+                Irbid City-Center
+              </label>
+              <button className="order">submit</button>
+            </form>
+          </section>
+        </main>
+        
+      </>
     );
   }
 }
